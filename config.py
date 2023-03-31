44fe0067880fff73fce5ff94ccecefe6f58de7c7
michael3735039 <89645088+michael3735039@users.noreply.github.com>
2023-03-31 07:17:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 
 
 class Config(object):
-    APP_ID = int(os.environ.get("APP_ID", 1234567))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    APP_ID = int(os.environ.get("APP_ID", 1293082))
+    API_HASH = os.environ.get("API_HASH", "0b113c40038ed82cea13be3eeaa2b0e6")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6145911399:AAGbx91MAd_paiRd7BW_7hkm9v97n9D6Bws")
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "705793735").split())
     IS_PUBLIC_BOT = os.environ.get("IS_PUBLIC_BOT") in ["True", "true"]
     LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL")) if os.environ.get("LOGS_CHANNEL") else None
     # DON'T CHANGE THESE 2 VARS
